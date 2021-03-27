eea4e08909cd8a5589b01707946783cfb221dcb1
Enrique Green <47195508+BruceManuelwayne@users.noreply.github.com>
2021-03-26 21:59:26 -0300

    ejercicio6
    clase

---
+++ Ejercicio6/Ejercicio6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo tecla;
+            Console.TreatControlCAsInput = true;
+
+            do
+            {
+                tecla = Console.ReadKey();
+                Console.WriteLine();
+                if((tecla.Key == ConsoleKey.F) &&
+                    ((tecla.Modifiers & ConsoleModifiers.Shift)!=0) &&
+                    ((tecla.Modifiers & ConsoleModifiers.Control) != 0))
+                    {
+                    break; 
+                    }
+                        
+
+            } while (true);
+        }
+    }
+}
